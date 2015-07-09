1d05d9b946f767050458a0f479a610219d327c0c
Caio Orofino <brazilianboy10@Caios-MacBook-Pro.local>
2015-07-09 14:50:46 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 WeatherWidget::Application.routes.draw do
-  get "widget/index"
+  root 'widget#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +40,7 @@ WeatherWidget::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
