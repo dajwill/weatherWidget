dc784a6e5a382b9e80451c0c1bd810b69d6303df
Caio Orofino <brazilianboy10@Caios-MacBook-Pro.local>
2015-07-10 08:19:12 -0700

    allow site to be embedded in iframe

--- app/controllers/widget_controller.rb
+++ app/controllers/widget_controller.rb
@@ -1,9 +1,19 @@
 class WidgetController < ApplicationController
   include WidgetHelper
+  after_action :allow_iframe, only: :embed
 
   def index
     @weather = get_weather
     @forecast = get_forecast
     @icon = get_icon(@weather)
+  end
+
+  def embed
+  end
+
+private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
